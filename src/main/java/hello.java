4b7724bdc87a767256625f2727c80158c9520226
lichungang <lichungang182719@126.com>
2025-11-27 16:32:57 +0800

    第一hello提交

---
+++ src/main/java/hello.java
@@ -0,0 +1,13 @@
+/**
+ * @Description:
+ * @Author: Sky
+ * @Times : 2025/11/27 16:31
+ */
+public class hello {
+
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+
+
+}
